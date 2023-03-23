2e1f859909383109e4d5b6c4693aee92efed3ef1
7007singh <shw12021982@gmail.com>
2023-03-24 00:41:46 +0530

    [Shweta]Add: On Form Submit populate the Employee Payroll Data Object

--- Js/EmployeeRegister.js
+++ Js/EmployeeRegister.js
@@ -1,6 +1,7 @@
-let salary=document.querySelector('#salary');
-let output=document.querySelector('.SalaryOutput');
-output.textContent=salary.value;
-    salary.addEventListener('input',function(){
-            output.textContent=salary.value;
-        });
+
+let salary = document.querySelector('#salary');
+let output = document.querySelector('.SalaryOutput');
+output.textContent = salary.value;
+salary.addEventListener('input', function () {
+    output.textContent = salary.value;
+});
