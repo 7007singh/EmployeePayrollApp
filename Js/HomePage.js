af4792972cb1e909bdcddc59a03292c79dbc964d
7007singh <shw12021982@gmail.com>
2023-03-28 23:35:38 +0530

    [Shweta]Add: Ability to view Employee Payroll details in a Tabular Format from JSON Object

--- Js/HomePage.js
+++ Js/HomePage.js
@@ -13,31 +13,53 @@ const createInnerHtml = () => {
     <th>Start Date</th>
     <th>Action</th>
 </tr>`;
+    let employeeData = createEmployeePayrollJSON()[1];
     let htmlContent = `${headerContent}
 <tr>
-    <td><img class="profile" src="../assets/profile-images/Ellipse -5.png" alt="profilePic"></td>
-    <td>Mohan Roy</td>
-    <td>Male</td>
-    <td class="dept-level">Sales</td>
-    <td>350000</td>
-    <td>10-03-2023</td>
-    <td>
-        <img id="1" onclick="remove(this)" src="../assets/icons/delete-black-18dp.svg" alt="delete">
-        <img id="2" onclick="update(this)" src="../assets/icons/create-black-18dp.svg" alt="edit">
-    </td>
-</tr>
-<tr>
-    <td><img class="profile" src="../assets/profile-images/Ellipse -1.png" alt="profilePic"></td>
-    <td>Sanskriti</td>
-    <td>Female</td>
-    <td class="dept-level">Hr</td>
-    <td>450000</td>
-    <td>15-03-2023</td>
+    <td><img class="profile" src="${employeeData._profilePic}"></td>
+    <td>${employeeData._name}</td>
+    <td>${employeeData._gender}</td>
+    <td>${getDeptHtml(employeeData._department)}</td>    
+    <td>${employeeData._salary}</td>
+    <td>${employeeData._startDate}</td>
     <td>
         <img id="1" onclick="remove(this)" src="../assets/icons/delete-black-18dp.svg" alt="delete">
         <img id="2" onclick="update(this)" src="../assets/icons/create-black-18dp.svg" alt="edit">
     </td>
 </tr>`;
-
     document.querySelector('#table').innerHTML = htmlContent;
+}
+
+const getDeptHtml = (deptList) => {
+    let deptHtml = '';
+    for(const dept of deptList){
+        deptHtml = `${deptHtml} <div class="dept-lable">${dept}</div>`
+    }
+    return deptHtml;
+}
+
+const createEmployeePayrollJSON = () => {
+    let employeePayrollListLocal = [
+        {
+            _name: 'Radha Kumari',
+            _gender: 'Female',
+            _department: ['Sales', 'Finance'],
+            _salary: '352000',
+            _startDate: '20-03-2023',
+            _notes: '',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/profile-images/Ellipse -1.png'
+        },
+        {
+            _name: 'Rahul Kumar',
+            _gender: 'Male',
+            _department: ['Finance'],
+            _salary: '452000',
+            _startDate: '10-03-2023',
+            _notes: '',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/profile-images/Ellipse -5.png'
+        }
+    ];
+    return employeePayrollListLocal;
 }
